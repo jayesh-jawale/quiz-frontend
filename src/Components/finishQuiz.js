1ac1a6c1126f65ce6b480b9efb7eee33c6a66c06
jayesh-jawale <jawalejayesh123@gmail.com>
2022-08-17 00:12:29 +0530

    some changes

--- src/Components/finishQuiz.js
+++ src/Components/finishQuiz.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 import { quizContext } from "../App";
 
 export function FinishQuiz() {
   const { score } = React.useContext(quizContext);
 
-  return <div className="end-quiz">Hi , your score is : {score}</div>;
+  const history = useHistory();
+
+  const logMeOut = () => {
+    sessionStorage.clear();
+    history.push('/')
+  }
+
+  return (
+    <React.Fragment>
+      <div className="end-quiz">
+        <div>Hi , your score is : {score}</div>
+        <div className="end-quiz-buttons">
+          <Button onClick={() => history.push('/select-course')} variant="outline-primary">Retake Quiz</Button>
+          <Button onClick={() => logMeOut()} variant="outline-primary">Logout</Button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
